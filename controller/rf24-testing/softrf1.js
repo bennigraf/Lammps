051a900d318caf74e556dfc309550397eccadd65
Benjamin Graf <bennigraf@gmail.com>
2013-10-31 16:54:34 +0100

    softrf24 testing...

--- controller/rf24-testing/softrf1.js
+++ controller/rf24-testing/softrf1.js
@@ -11,6 +11,13 @@ rf.setChannel(0);
 rf.setRate(2); // 2mbit
 rf.setPower(3); // 0dbm
 rf.setCRC(1, 1); // active, 2byte
+// ...more setup stuff if needed...
 
 rf.sendData(new Buffer([0x2d, 0x00, 0x00, 0xff]));
-// ...more setup stuff if needed...
+
+rf.startAutoMode();
+
+setTimeout(function(){
+	rf.sendToFifo(new Buffer([0x2d, 0x00, 0xff, 0x00]));
+	rf.sendToFifo(new Buffer([0x2d, 0x0f, 0x00, 0xf0]));
+}, 2030);
