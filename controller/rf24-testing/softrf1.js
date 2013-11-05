60f95420df28cc9bd3ed0f19739506d32936d21a
Benni Graf <bennigraf@gmail.com>
2013-11-05 10:40:10 +0000

    some testing...

--- controller/rf24-testing/softrf1.js
+++ controller/rf24-testing/softrf1.js
@@ -13,19 +13,22 @@ setTimeout(function(){
 	//rf.setChannel(0);
 	//rf.setRate(2); // 2mbit
 	//rf.setPower(3); // 0dbm
-	//rf.setCRC(1, 1); // active, 2byte
+	rf.setCRC(1, 0); // active, 2byte
 
 	//rf.sendData(new Buffer([0x2d, 0x00, 0x00, 0xff]));
 	// ...more setup stuff if needed...
 
 
-	rf.readRegister(0x00, 5, function(buf){
+	rf.readRegister(0x00, 1, function(buf){
 		console.log(buf);
 	});
+	rf.readRegister(0x10, 5, function(buf) { console.log(buf) });
+
+	rf.sendData(new Buffer([0x2d, 0xa0, 0x00, 0xff, 0xff]));
 }, 1000);
 
 
 setTimeout(function(){
-	rf.sendToFifo(new Buffer([0x2d, 0x00, 0xff, 0x00]));
-	rf.sendToFifo(new Buffer([0x2d, 0x0f, 0x00, 0xf0]));
+//	rf.sendToFifo(new Buffer([0x2d, 0x00, 0xff, 0x00]));
+//	rf.sendToFifo(new Buffer([0x2d, 0x0f, 0x00, 0xf0]));
 }, 2030);
