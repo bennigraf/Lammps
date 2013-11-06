887e82f0e27b13fede367a0609ae4ebb3ef2e643
Benjamin Graf <bennigraf@gmail.com>
2013-11-06 16:25:59 +0100

    modulemanager: fake modules to send out demo data...

--- controller/modulemngr/module.js
+++ controller/modulemngr/module.js
@@ -2,3 +2,39 @@
 Module-object, that holds and tracks properties and functionalities of a single module managed
 by modulemngr...
 */
+
+module.exports = Module;
+
+function Module (id, mngr) {
+	this.id = id;
+	this.address = 0;
+	this.functions = new Array();
+	
+	this.mngr = mngr; // reference to module manager, needed for callback to send data out
+	
+}
+
+
+Module.prototype.setData = function(fn, data) {
+	console.log(fn, data);
+	switch(fn) {
+		case "color":
+			this.sendColor(data);
+			break;
+	}
+}
+
+Module.prototype.sendColor = function(data) {
+	// Assuming data is (number)rgb (0..1)...
+	// make buffer out of it!!
+	function toInt(num) {
+		return Math.round(num*255);
+	}
+	var fsize = 0x00; // 1st byte of packet
+	var fn = 0x00; // 2nd byte of packet
+	var r = toInt(data[0]);
+	var g = toInt(data[1]); 
+	var b = toInt(data[2]);
+	var dataBuf = new Buffer([fsize, fn, r, g, b]);
+	this.mngr.sendData(this.address, dataBuf);
+}
--- controller/modulemngr/modulemngr.js
+++ controller/modulemngr/modulemngr.js
@@ -1,5 +1,6 @@
 
 var RF = require('../soft-rf24/soft-rf24.js');
+var Module = require('./module.js');
 
 // export stuff...
 module.exports = MM;
@@ -8,13 +9,54 @@ module.exports = MM;
 function MM () {
 	// setup module manager
 	
-	var rf = new RF("/dev/spidev0.0");
-	rf.startAutoMode();
+	this.rf = new RF("/dev/spidev0.0");
+	// rf.startAutoMode();
+	this.rf.setAddrWidth(5);
+	this.rf.setTxAddress(0);
+	this.rf.setChannel(0);
+	this.rf.setAutoAck(0, 1);
+	this.rf.setRate(2);
+	this.rf.setPower(3);
+	this.rf.setCRC(1, 0);
+	this.rf.setRXTX(0);
+	
+	this.modules = new Array();
+
+
+
+	this.modules["xhhe"] = new Module("xhhe", this);
+	this.modules["xhhe"].address = 8859435828;
+	this.modules["xhhe"].functions = ["color"];
+	
+	this.modules["ddwg"] = new Module("ddwg", this);
+	this.modules["ddwg"].address = 13154403124;
+	this.modules["ddwg"].functions = ["color"];
+	
+	this.modules["askw"] = new Module("askw", this);
+	this.modules["askw"].address = 17449370420;
+	this.modules["askw"].functions = ["color"];
+	
+	this.modules["icos"] = new Module("icos", this);
+	
+	
+		// { title: 'module1', id: 'xhhe', 'location': [0, 0], 'functions': ["dim", "color"] },
+		// { title: 'module2', id: 'ddwg', 'location': [0, 1], 'functions': ["dim", "color"] },
+		// { title: 'module3', id: 'askw', 'location': [1, 0], 'functions': ["dim"] },
+		// { title: 'module4', id: 'icos', 'location': [1, 0], 'functions': ["sound"] },
+	// ];
 }
 
 MM.prototype.boot = function () {
-	
+	return false;
 }
+
+MM.prototype.sendData = function(addr, data) {
+	this.rf.setTxAddress(addr);
+	this.rf.setRXTX(1);
+	console.log(data);
+	this.rf.sendData(data);
+}
+
 
 
 /*
