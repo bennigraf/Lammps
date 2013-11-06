ad07947fc88a7aaa9e18534d1c5c3325f914fd63
Benjamin Graf <bennigraf@gmail.com>
2013-11-06 01:05:05 +0100

    removed logging from fake-gpio/-spi

--- controller/soft-rf24/fake-gpio.js
+++ controller/soft-rf24/fake-gpio.js
@@ -1,15 +1,15 @@
 
 // fake gpio module providing the right hooks to work without the hardware being available
 function Gpio() {
-	console.log("==Fake GPIO inited!");
+	// console.log("==Fake GPIO inited!");
 }
 
 Gpio.prototype.setup = function() {
-	console.log("==Fake GPIO setup call");
+	// console.log("==Fake GPIO setup call");
 }
 
 Gpio.prototype.write = function(pin, val, callback) {
-	console.log("==write gpio: "+pin+", "+val);
+	// console.log("==write gpio: "+pin+", "+val);
 	if(callback) { callback(); }
 }
 
--- controller/soft-rf24/fake-spi.js
+++ controller/soft-rf24/fake-spi.js
@@ -1,22 +1,22 @@
 
 // fake spi module providing the right hooks to work without the hardware being available
 function Spi () {
-	console.log('==inited fake spi connection to path');
+	// console.log('==inited fake spi connection to path');
 }
 
 Spi.prototype.open = function () {
-	console.log("==opened fake spi...");
+	// console.log("==opened fake spi...");
 }
 
 Spi.prototype.write = function(data, callback) {
-	console.log("==writing...");
+	// console.log("==writing...");
 	console.log(data);
 	callback();
 }
 
 Spi.prototype.transfer = function(txbuf, rxbuf, callback) {
-	console.log("==transferring...");
-	console.log(txbuf);
+	// console.log("==transferring...");
+	// console.log(txbuf);
 	callback('dev', txbuf);
 }
 
