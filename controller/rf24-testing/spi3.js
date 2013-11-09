a78a05637f8a830a5cbe093d3d56a66cae955c71
Benni Graf <bennigraf@gmail.com>
2013-11-09 16:28:13 +0000

    fixig spi rf test, works now\!

--- controller/rf24-testing/spi3.js
+++ controller/rf24-testing/spi3.js
@@ -44,6 +44,7 @@ function setNrfReg(addr, val) {
 };
 
 
+var LOUD = false;
 
 
 var spi = new SPI.Spi('/dev/spidev0.0', {
@@ -74,38 +75,41 @@ function sendstuff() {
 			function sendbuf (comment, bufarr) {
 				if(typeof comment !== 'string' && bufarr === undefined) { 
 					bufarr = comment; 
-					console.log("=== sending");
+					if(LOUD) { console.log("=== sending"); }
 				} else {
-					console.log("=== " + comment);
+					if(LOUD) { console.log("=== " + comment); }
 				}
 				var txbuf = new Buffer(bufarr);
 				var rxbuf = txbuf;
-				console.log(txbuf);
-				spi.transfer(txbuf, rxbuf, function(dev, buf){ console.log(buf) });
+				if(LOUD) { console.log(txbuf); }
+				spi.transfer(txbuf, rxbuf, function(dev, buf){ 
+					if(LOUD) { console.log(buf) }
+				});
 			}
 
 			// enable aa
-			sendbuf("enable aa", [ 0x20 + 0x01, 0x01 ]);
+			// sendbuf("enable aa", [ 0x20 + 0x01, 0x01 ]);
 			// disable aa (compatibility with nrf24l01?)
-			// sendbuf("disable aa", [ 0x20 + 0x01, 0x00]);
+			sendbuf("disable aa", [ 0x20 + 0x01, 0x00]);
 			
 			// enable recv address
 			sendbuf("enable rcv pipe", [ 0x20 + 0x02, 0x01 ]);
 			
 			// rf channel
-			sendbuf("set rf channel", [ 0x20 + 0x05, 0x00 ]);
+			sendbuf("set rf channel", [ 0x20 + 0x05, 0x02 ]);
+			
 			// RX_PW_P0 (rx payload...)
-			sendbuf([ 0x20 + 0x11, 0x11 ]); // 17 byte pw
+			sendbuf([ 0x20 + 0x11, 0x20 ]); // 16 byte pw
 			
 			// RF_SETUP:
 			// setup: 1mbit + 0dbm
-			sendbuf("1mbit + 0dbm", [ 0x20 + 0x06, 0x07 ]);
+			// sendbuf("1mbit + 0dbm", [ 0x20 + 0x06, 0x07 ]);
+			// 1mbit -18dbm
+			// sendbuf("1mbit -18dbm", [ 0x20 + 0x06, 0x00 ]);
 			// 2mbit 0dbm
-			// sendbuf("2mbit + 0dbm", [ 0x20 + 0x06, 0x0e]);
+			sendbuf("2mbit + 0dbm", [ 0x20 + 0x06, 0x0e]);
 			// 2mbit -18dbm:
 			// sendbuf("2mbit -18dbm", [ 0x20 + 0x06, 0x08 ]);
-			// 1mbit -18dbm
-			sendbuf("1mbit -18dbm", [ 0x20 + 0x06, 0x00 ]);
 			
 			// CONFIG
 			// crc, 2byte, pwrup, trx
@@ -122,11 +126,16 @@ function sendstuff() {
 			//// ch0, 1mbit, crc 2byte
 			//// oooder ch40, 2mbit, crc 2byte
 			
+			// dpl!!!
+			sendbuf("enabling dpl", [0x20 + 0x1d, 0x05]); // globally enables dpl
+			sendbuf("enabling dpl on pipes", [0x20 + 0x1c, 0x3f]); // enables dpl on all pipes
+			
 			
 			// rx addr (again?)
 			sendbuf("rxaddr", [ 0x20 + 0x0A, 0x34, 0x43, 0x10, 0x10, 0x04 ]);
 			// tx addr
-			sendbuf("tx addr", [ 0x30, 0x34, 0x43, 0x10, 0x10, 0x03 ]);
+			sendbuf("tx addr", [ 0x30, 0x34, 0x43, 0x10, 0x10, 0x04 ]);
+			// sendbuf("tx addr", [ 0x30, 0xe7, 0xe7, 0xe7, 0xe7, 0xe8 ]);
 			
 			// clock out status thingy, just for checking...
 			sendbuf("check status...", [ 0x00 + 0x07, 0x00]);
@@ -138,7 +147,10 @@ function sendstuff() {
 			sendbuf("check aa", [0x00 + 0x01, 0x00]);
 			// check dpl
 			sendbuf("check dpl", [0x00 + 0x1d, 0x00]);
-			
+			// check rxaddr
+			sendbuf("check rxaddr", [0x10, 0x00, 0x00, 0x00, 0x00, 0x00]);
+			// check chnl
+			sendbuf("check channel", [0x00 + 0x05, 0x00]);
 			
 			var i = 0;
 			// setInterval(function(){
@@ -157,26 +169,28 @@ function sendstuff() {
 			
 			setInterval(function() {
 				//check fifo...
-				sendbuf("check fifo", [0x00 + 0x17, 0x00 ]);
+				// sendbuf("check fifo", [0x00 + 0x17, 0x00 ]);
 				
 				var r = (((i % 3) == 0) & 1) * 250/10;
 				var g = (((i % 3) == 1) & 1) * 250/10;
 				var b = (((i % 3) == 2) & 1) * 250/10;
-				var buf = new Buffer(17);
-				buf.fill(0x00);
-				buf[2 + i%15] = 0xf0;
+				// var buf = new Buffer(17);
+				var buf = new Buffer(8);
+				buf.fill(0x30);
+				buf[0] = 0xa0;
+				buf[1 + i%7] = 0xf0;
 				sendbuf(buf);
 				// sendbuf([ 0x00, 0x00, 0x0+r, 0x0+g, 0x0+b, 0x0f, 0xf0, 0xf0, 0xf0, 0xf0, 0xf0, 0xf0, 0xf0, 0xf0, 0xf0, 0xf0, 0x0f ]);
 				//sendbuf([ 0x2d, 0x0+r, 0x0+g, 0x0+b ]);
 				ce(1, function(){ 
 					/*console.log("ende");*/ 
 					// sendbuf([ 0x27, 0xff ]);
-					// ce(0); 
-					setTimeout(function(){ ce(0) }, 10);
+					ce(0); 
+					// setTimeout(function(){ ce(0) }, 10);
 				});
 				
 				i = i + 1;
-			}, 50);
+			}, 100);
 
 
 		// }, 100);
