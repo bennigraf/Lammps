a586d3ed04c04e3453a4500789b7792fa54a84d6
Benni Graf <bennigraf@gmail.com>
2014-06-17 19:41:00 +0000

    testcase

--- controller/lammpsmngr/nrfTest.js
+++ controller/lammpsmngr/nrfTest.js
@@ -1,11 +1,13 @@
-var radio = require('nrf').connect(spiDev, cePin, irqPin);
+var spidev = "/dev/spidev0.0", cePin = 24, irqPin = 25;
+var radio = require('nrf').connect(spidev, cePin, irqPin);
 radio.channel(0x7d).dataRate('2Mbps').crcBytes(2).autoRetransmit({count:15, delay:4000});
 radio.begin(function () {
     var rx = radio.openPipe('rx', new Buffer([0x00, 0x00, 0x00, 0x00, 0x00]));
         // tx = radio.openPipe('tx', 0xF0F0F0F0D2);
+	radio.printDetails();
 	rx.on('readable', function() {
 		console.log("reading data", rx.read());
 		radio.printDetails();
-	}
-    rx.pipe(tx);        // echo back everything
+	});
+    //rx.pipe(tx);        // echo back everything
 });
